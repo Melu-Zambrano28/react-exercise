4b8b9a94e7455ea9af05895525b6042f30ad96d3
Melissa zambrano <belenzambrano@hotmail.es>
2019-09-19 11:27:17 +0200

    Ex2: Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. Instead it only renders three Part components of which each renders the name and number of exercises of one part

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ let Total = (props) =>{
   return <p>{props.sum}</p>
 }
 
+let Part = (props)=>{
+  return (
+    <div>
+      <p>
+        <Content part={props.part} content={props.exercises}/>
+      </p></div>);
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -28,8 +36,19 @@ const App = () => {
      <Content part={part1} content={exercises1}/>
      <Content part= {part2} content={exercises2}/>
      <Content part={part3} content={exercises3}/>
+     <p>Number of exercises <Total sum ={exercises1 + exercises2 + exercises3}/></p>
+
+     <h1>Exercises 2: </h1>
+     <p>Refactor the Content component so that it does not render any names of parts or their number of exercises by itself.<br></br>
+        Instead it only renders three Part components of which each renders the name and number of exercises of one part
+      </p>
+
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+      
     
-      <p>Number of exercises <Total sum ={exercises1 + exercises2 + exercises3}/></p>
+   
     </div>
   )
 }
