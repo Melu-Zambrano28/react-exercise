b7278ce9723630eaeef59bdcf9f765c266733b9e
Melissa zambrano <belenzambrano@hotmail.es>
2019-09-19 11:38:45 +0200

    Description of the exercises.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let Header = (props)=>{
 }
 
 let Content = (props)=>{
-  return <p>Part of course :{ props.part} context :{props.content}</p>
+  return <p>Part of course :{ props.part} count :{props.content}</p>
 }
 
 let Total = (props) =>{
@@ -32,13 +32,18 @@ const App = () => {
 
   return (
     <div>
+    <h1>Exercise 1: </h1>
+    <p>
+    Refactor the code so that it consists of three new components: Header, Content, and Total.<br>
+    </br> All data still resides in the App component, which passes the necessary data to each component using props.<br></br> Header takes care of rendering the name of the course, Content renders the parts and their number of exercises and Total renders the total amount of exercises.
+    </p>
      <Header course={course} />
      <Content part={part1} content={exercises1}/>
      <Content part= {part2} content={exercises2}/>
      <Content part={part3} content={exercises3}/>
      <p>Number of exercises <Total sum ={exercises1 + exercises2 + exercises3}/></p>
 
-     <h1>Exercises 2: </h1>
+     <h1>Exercise 2: </h1>
      <p>Refactor the Content component so that it does not render any names of parts or their number of exercises by itself.<br></br>
         Instead it only renders three Part components of which each renders the name and number of exercises of one part
       </p>
